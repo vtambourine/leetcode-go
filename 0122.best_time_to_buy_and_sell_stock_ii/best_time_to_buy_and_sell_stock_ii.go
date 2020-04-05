9baa34a633236afc25887849eb150b10bb10528f
Benjamin T <vtambourine@ya.ru>
2020-04-05 12:22:07 +0300

    122. Best Time to Buy and Sell Stock II

---
+++ 0122.best_time_to_buy_and_sell_stock_ii/best_time_to_buy_and_sell_stock_ii.go
@@ -0,0 +1,11 @@
+package best_time_to_buy_and_sell_stock_ii
+
+func maxProfit(prices []int) int {
+	profit := 0
+	for i := 0; i < len(prices)-1; i++ {
+		if prices[i] < prices[i+1] {
+			profit += prices[i+1] - prices[i]
+		}
+	}
+	return profit
+}
---
+++ 0122.best_time_to_buy_and_sell_stock_ii/best_time_to_buy_and_sell_stock_ii_test.go
@@ -0,0 +1,22 @@
+package best_time_to_buy_and_sell_stock_ii
+
+import "testing"
+
+type test struct {
+	input  []int
+	expect int
+}
+
+func TestTemplate(t *testing.T) {
+	tests := []test{
+		{[]int{7, 1, 5, 3, 6, 4}, 7},
+		{[]int{1, 2, 3, 4, 5}, 4},
+		{[]int{7, 6, 4, 3, 1}, 0},
+	}
+
+	for _, c := range tests {
+		if result := maxProfit(c.input); result != c.expect {
+			t.Fatalf("maxProfit(%v) fails.\nExpected %v\nReceived %v", c.input, c.expect, result)
+		}
+	}
+}
