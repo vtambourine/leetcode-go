c83a46de30f2dc9a3ca55820ec3e5b92f6e2db06
Benjamin T <vtambourine@ya.ru>
2020-04-25 01:35:27 +0300

    146. LRU Cache

--- 0146.lru_cache/lru_cache.go
+++ 0146.lru_cache/lru_cache.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package lru_cache
 
 type LRUCache struct {
 	cache    map[int]int
@@ -26,50 +24,26 @@ func (c *LRUCache) Get(key int) int {
 			}
 		}
 		return v
-	} else {
-		return -1
 	}
+	return -1
 }
 
 func (c *LRUCache) Put(key int, value int) {
-	//fmt.Println("Put", key, value)
 	if _, ok := c.cache[key]; ok {
-		//fmt.Println("+ hit cache", c.cache)
 		c.cache[key] = value
 		for i, k := range c.queue {
 			if k == key {
 				c.queue = append(c.queue[:i], c.queue[i+1:]...)
-				c.queue = append(c.queue, key)
 				break
 			}
 		}
 	} else {
-		//fmt.Println("+ miss cache", c.cache)
 		c.cache[key] = value
-		c.queue = append(c.queue, key)
-		if len(c.queue) > c.capacity {
+		if len(c.queue) == c.capacity {
 			old := c.queue[0]
 			delete(c.cache, old)
 			c.queue = c.queue[1:]
 		}
 	}
-}
-
-func main() {
-	cache := Constructor(2)
-	//cache.Put(1, 1)
-	//cache.Put(2, 2)
-	//fmt.Println(cache.Get(1)) // returns 1
-	//cache.Put(3, 3)  // evicts key 2
-	//fmt.Println(cache.Get(2)) // returns -1 (not found)
-	//cache.Put(4, 4)  // evicts key 1
-	//fmt.Println(cache.Get(1)) // returns -1 (not found)
-	//fmt.Println(cache.Get(3)) // returns 3
-
-	cache.Put(2, 1)
-	cache.Put(2, 2)
-	fmt.Println(cache.Get(2)) // 2
-	cache.Put(1, 1)
-	cache.Put(4, 1)
-	fmt.Println(cache.Get(2)) // -1
+	c.queue = append(c.queue, key)
 }
--- 0146.lru_cache/lru_cache_test.go
+++ 0146.lru_cache/lru_cache_test.go
@@ -1,18 +1,49 @@
-package main
+package lru_cache
 
-import "testing"
+import (
+	"strconv"
+	"strings"
+	"testing"
+)
 
 type test struct {
-	input  []int
-	expect int
+	methods []string
+	args    [][]int
+	expect  string
 }
 
-func TestTemplate(t *testing.T) {
-	tests := []test{}
+func TestLRUCache(t *testing.T) {
+	tests := []test{
+		{
+			[]string{"LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"},
+			[][]int{{2}, {1, 1}, {2, 2}, {1}, {3, 3}, {2}, {4, 4}, {1}, {3}, {4}},
+			"null,null,null,1,null,-1,null,-1,3,4",
+		},
+		{
+			[]string{"LRUCache", "put", "put", "get", "put", "put", "get"},
+			[][]int{{2}, {2, 1}, {2, 2}, {2}, {1, 1}, {4, 1}, {2}},
+			"null,null,null,2,null,null,-1",
+		},
+	}
 
 	for _, c := range tests {
-		if result := solution(c.input); result != c.expect {
-			t.Fatalf("solution(%v) fails.\nExpected %v\nReceived %v", c.input, c.expect, result)
+		output := make([]string, len(c.methods))
+		var cache LRUCache
+		for i := range c.methods {
+			switch c.methods[i] {
+			case "LRUCache":
+				cache = Constructor(c.args[i][0])
+				output[i] = "null"
+			case "put":
+				cache.Put(c.args[i][0], c.args[i][1])
+				output[i] = "null"
+			case "get":
+				output[i] = strconv.Itoa(cache.Get(c.args[i][0]))
+			}
+		}
+		result := strings.Join(output, ",")
+		if result != c.expect {
+			t.Fatalf("LRUCache failed.\nInput\n%v\n%v\nExpected %v\nReceived %v", c.methods, c.args, c.expect, result)
 		}
 	}
 }
