22d4004ae61893078cffbbe24f58559fc546c01f
Benjamin T <vtambourine@ya.ru>
2020-12-02 00:32:12 +0300

    104. Maximum Depth of Binary Tree

---
+++ 0104.maximum_depth_of_binary_tree/maximum_depth_of_binary_tree.go
@@ -0,0 +1,37 @@
+package maximum_depth_of_binary_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func nodeDepth(node *TreeNode, depth int) int {
+	if node.Left == nil && node.Right == nil {
+		return depth + 1
+	}
+
+	var leftDepth, rightDepth int
+	if node.Left != nil {
+		leftDepth = nodeDepth(node.Left, depth + 1)
+	}
+	if node.Right !=nil {
+		rightDepth = nodeDepth(node.Right, depth + 1)
+	}
+
+	return max(leftDepth, rightDepth)
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return nodeDepth(root, 1)
+}
---
+++ 0104.maximum_depth_of_binary_tree/maximum_depth_of_binary_tree_test.go
@@ -0,0 +1,18 @@
+package maximum_depth_of_binary_tree
+
+import "testing"
+
+type test struct {
+	input  []int
+	expect int
+}
+
+func TestTemplate(t *testing.T) {
+	tests := []test{}
+
+	for _, c := range tests {
+		if result := solution(c.input); result != c.expect {
+			t.Fatalf("solution(%v) fails.\nExpected %v\nReceived %v", c.input, c.expect, result)
+		}
+	}
+}
